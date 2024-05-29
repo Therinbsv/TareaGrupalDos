a5a8f42c76f27c52b6d584a8b85bdd1345d23e63
Therinbsv <katherine.sanchez@epn.edu.ec>
2024-05-29 16:19:35 -0500

    Series Katherine Sanchez

--- src/App.java
+++ src/App.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 public class App {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        int KsNroTermino = 0; 
-        Katherin ks = new Katherin();
-        System.out.println("~~~~~~~~~~~SERIES NUMERICAS~~~~~~~~~~~");
+        int KsNroTermino = 0, KsNroTermino2 = 0; 
+        String skCaracter;
+        KatherineSanchez ks = new KatherineSanchez();
+        System.out.println("~~~~~~~~~~~Bienvenido~~~~~~~~~~~");
+        System.out.print("Ingrese caracter con el que se formaran las figuras: ");
+        skCaracter = sc.nextLine();
         do {
             try {
                 System.out.print("Ingrese cantidad de terminos que desea ver: ");
@@ -14,19 +17,18 @@ public class App {
                 System.out.println("***Asegurese que el valor ingresado sea correcto***");
                 sc.next();
             }
-        } while (KsNroTermino<=0);
-
+        } while (KsNroTermino <= 0);
+        System.out.println("~~~~~~~~~~~SERIES NUMERICAS~~~~~~~~~~~");
         ks.ksSN1(KsNroTermino);
-        ks.ksSN2(KsNroTermino);
-        ks.ksSN3(KsNroTermino);
-        ks.ksSN4(KsNroTermino);
         ks.ksSN5(KsNroTermino);
-        ks.ksSN6(KsNroTermino);
-        ks.ksSN7(KsNroTermino);
-        ks.ksSN8(KsNroTermino);
         ks.ksSN9(KsNroTermino);
-        ks.ksSN10(KsNroTermino);
-        ks.ksSN11(KsNroTermino);
-        
+        System.out.println("~~~~~~~~~~~SERIES DE CARACTERES~~~~~~~~~~~");
+        ks.ksSC3(KsNroTermino);
+        ks.ksSC7(KsNroTermino);
+        System.out.println("~~~~~~~~~~~FIGURAS~~~~~~~~~~~");
+        ks.ksFigura4(KsNroTermino, skCaracter);
+        ks.ksFigura8(KsNroTermino);
+        ks.ksFigura12(KsNroTermino);
+        ks.ksFigura16(KsNroTermino);
     }
 }
